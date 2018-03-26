917196df9481157948f3308f5a87c503639af528
Tevin Achong <tevinachong@yahoo.com>
2018-03-26 16:40:54 -0400

    Displaying anime info

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from firstApp.models import Topic, Webpage, AccessRecord, User
+from firstApp.models import Topic, Webpage, AccessRecord, User, Anime
 # Create your views here.
 
 def index(request):
-    webpages_list = AccessRecord.objects.order_by('date')
-    date_dict = {'access_records' : webpages_list}
+    anime_list = Anime.objects.order_by('name')
+    anime_dict = {'anime' : anime_list}
 
-    return render(request, 'firstApp/index.html', context = date_dict)
+    return render(request, 'firstApp/index.html', context = anime_dict)
 
 def users(request):
     userList = User.objects.order_by('lastName')
