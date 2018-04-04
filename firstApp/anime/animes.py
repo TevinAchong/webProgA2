424ab7dcc8d8e4d74b193cc694a812817dfb00c9
jim <jimmelgreer23@gmail.com>
2018-04-04 10:21:54 -1100

    Forms

---
+++ firstApp/anime/animes.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Anime(models.Model):
+    animeId = models.CharField(max_length = 100,unique = True) ##Ideally should be unique
+    name = models.CharField(max_length = 500)
+    genre = models.CharField(max_length = 500)
+    animeType = models.CharField(max_length = 500)
+    episodes = models.CharField(max_length = 10)
+    rating = models.CharField(max_length = 500)
+    members = models.CharField(max_length = 500)
+    #poster = models.ImageField() ##To store images retrieved by Google Search
+    #----All data fields are stored as CharField due to possible inconsistencies in anime.csv file ---#
+    
+    def __str__(self):
+        return str(self.name)
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from firstApp.models import Anime
-from . import forms
+from django.http import HttpResponseRedirect
+from .anime import Anime
+from .anime import animeForm
 # Create your views here.
 
 def index(request):
@@ -12,19 +12,22 @@ def index(request):
 
 
 def form_name_view(request):
-    form = forms.animeForm()
+    form = animeForm()
     
     if request.method == 'POST':
-        form = forms.animeForm(request.POST)
+        form = animeForm(request.POST)
 
         if form.is_valid():
-            print("VALIDATION SUCCESS")
-            print("id: " +form.cleaned_data['animeID'])
-            print("Name: " +form.cleaned_data['name'])
-            print("Genre: " +form.cleaned_data['genre'])
-            print("Type: " +form.cleaned_data['Atype'])
-            print("Episodes: " +form.cleaned_data['episodes'])
-            print("Rating: " +form.cleaned_data['rating'])
-            print("Members: " +form.cleaned_data['members'])
+            anime = Anime()
+            anime.animeId =  form.cleaned_data['animeId']
+            anime.name = form.cleaned_data['name']
+            anime.genre = form.cleaned_data['genre']
+            anime.animeType = form.cleaned_data['animeType']
+            anime.episodes = form.cleaned_data['episodes']
+            anime.rating = form.cleaned_data['rating']
+            anime.members = form.cleaned_data['members']
+
+            form.save()
+            return HttpResponseRedirect("/")
 
     return render(request, 'firstApp/form.html', {'form': form})
